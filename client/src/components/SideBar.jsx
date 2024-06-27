3e8180cd51a32403857e7d6625e1cecc1aeb3f88
Nishant <nishant040305@gmail.com>
2024-06-27 16:50:42 +0530

    feat: add SideBar component in UI layer

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -2,3 +2,34 @@ import React from "react";
 import '.././assets/SideBar.css';
 
 
+const SideBar =()=>{
+    return(
+        <div className="sideBar">
+            <div>
+            <img className="rounded-circle profilePic" src="https://lh3.googleusercontent.com/a/ACg8ocLIiWPNraDN3nfZ7rpQjGFqdpcpwE9ugPxL5VmVupt9KL5Rgg5Y=s360-c-no"></img>
+            <div style={{fontSize: 30}}>Nishant Mohan</div>
+            </div>
+            
+            <div className="container"style={{fontSize: 24,alignContent:"center"}} >
+                <ul>
+                    <li>
+                        Profile
+                    </li>
+                    <li>
+                        Trending Stocks
+                    </li>
+                    <li>
+                        Portfolio
+                    </li>
+                    <li>
+                        History
+                    </li>
+                    
+                </ul>
+            </div>
+            <div>Help Centre</div>
+        </div>
+    )
+}
+
+export default SideBar;
