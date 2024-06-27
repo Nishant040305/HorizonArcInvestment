92c4d476054ab29a6c2cc082ca27d1fd65b4f1e4
Nishant <nishant040305@gmail.com>
2024-06-27 15:38:11 +0530

    add: Add sidebar UI and integrated Tailwind Css
    Sidebar UI is added includes basic data and Tailwind Css and Bootstrap
    integrated into the client

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -1,4 +1,35 @@
 import React from "react";
 import '.././assets/SideBar.css';
+const SideBar =()=>{
+    return(
+        <div className="sideBar">
+            <div>
+            <img className="rounded-circle profilePic" src="https://lh3.googleusercontent.com/a/ACg8ocLIiWPNraDN3nfZ7rpQjGFqdpcpwE9ugPxL5VmVupt9KL5Rgg5Y=s360-c-no"></img>
+            <div style={{fontSize: 30}}>Nishant Mohan</div>
+            </div>
+            <div>
+            <div className="container"style={{fontSize: 24,alignContent:"center"}} >
+                <ul style={{textAlign:"left"}}>
+                    <li>
+                        Profile
+                    </li>
+                    <li>
+                        Trending Stocks
+                    </li>
+                    <li>
+                        Portfolio
+                    </li>
+                    <li>
+                        History
+                    </li>
+                    
+                </ul>
+            </div>
+            </div>
+            
+            <div style={{fontSize: 25 ,display:"flex" , flexDirection:"row",alignItems: "center"}}><img src="viber_152851.png" className="w-6 h-6" ></img><spam style={{margin:15}}>Help Centre</spam></div>
+        </div>
+    )
+}
 
-
+export default SideBar;
--- client/src/containers/App.jsx
+++ client/src/containers/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from '.././assets/react.svg'
 import viteLogo from '/vite.svg'
 import '.././assets/App.css'
 import { Route, Routes } from 'react-router-dom';
-import Login from '../components/Login';
+import Login from '.././components/Login';
+import SideBar from '../components/sideBar';
 function App() {
 
   return (
     <Routes>
       <Route path="/" element={<Login></Login>}></Route>
+      <Route path="/sideBar" element={<SideBar></SideBar>}></Route>
     </Routes>
     
   );
