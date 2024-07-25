c93f6071fc475f7a79b3c62c3b5defa622b9ccb0
Nishant <nishant040305@gmail.com>
2024-07-25 12:03:25 +0530

    feat: add friend request to the global user
    Friend request is send using socket.io with
    message @friend-request/send from frontend to the backend

--- client/src/components/DashboardComponent/FindPeople/FindPeople.jsx
+++ client/src/components/DashboardComponent/FindPeople/FindPeople.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import './FindPeople.css';
+import {socket} from '../../../Lib/socket';
 const FindPeopleBlock =(props)=>{
+  const user = useSelector(state=>state.user);
+  const sendFriendRequest =(receiverId)=>{
+    socket.emit('friend-request/send',{SenderId:user._id,receiverId:receiverId,message:{
+      image:user.image,
+      Username:user.Username,
+    },NotifType:"friend-request/send"});
+    socket.on('Notification',(Data)=>{
+      console.log(Data);
+    })
+  }
     return(
-        <div className='FindPeopleBlock'>
+        <div className='FindPeopleBlock' onClick={()=>{sendFriendRequest(props._id)}}>
           <div className='Findpeople-name'>
           <img className="rounded-full w-10 h-10 mr-10 ml-3" src={props.image}></img> 
           <div className=''>{props.name}</div>
@@ -25,7 +36,8 @@ const FindPeople = () => {
       {globalUser.map((info, index) => (
               
               <FindPeopleBlock 
-                  key={info._id || index} 
+                  key={info._id||index} 
+                  _id = {info._id}
                   image={info.image}
                   name={info.Username}
               />
