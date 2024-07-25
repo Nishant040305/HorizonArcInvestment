430e02a6c3affa291b7af83e1917fe5a1dbd42b1
Nishant <nishant040305@gmail.com>
2024-07-25 16:41:15 +0530

    feat: add chatRoom and Shortlist Array
    chatRoom to store the chatting room and shortlist to store the
    shortlisted products

--- server/models/user.js
+++ server/models/user.js
@@ -51,6 +51,10 @@ const userSchema = new Schema(
             type:String,
             default:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
         },
+        ShortList:{
+            type:[Object],
+            default:[],
+        },
         StocksHold:{
             type: [
                 {
@@ -64,6 +68,10 @@ const userSchema = new Schema(
                     },
                 },
             ],
+        },
+        chatRoom:{
+            type:[String],
+            default:[]
         }
     }
 )
