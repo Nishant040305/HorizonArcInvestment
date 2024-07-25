f1212291f3e0dc5469848041346bf60742488a2e
Nishant <nishant040305@gmail.com>
2024-07-25 11:57:13 +0530

    feat: add calculateDistance func to calc. distance
    This function calculates the geographyical distance between
    two points using there geographyical location.

--- client/src/Lib/ImportantFunc.js
+++ client/src/Lib/ImportantFunc.js
@@ -17,3 +17,20 @@ export const numFormat =(num)=>{
     const numberToDisplay = new Intl.NumberFormat().format(num);
     return numberToDisplay;
 }
+
+export const calculateDistance=(lat1, lon1, lat2, lon2)=> {
+    const R = 6371; // Earth's radius in kilometers
+
+    const degToRad = (deg) => (deg * Math.PI) / 180;
+    const dLat = degToRad(lat2 - lat1);
+    const dLon = degToRad(lon2 - lon1);
+
+    const a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(degToRad(lat1)) * Math.cos(degToRad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const distance = R * c;
+
+    return distance;
+}
