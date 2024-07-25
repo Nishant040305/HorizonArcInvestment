9e314346262ba34426362c20046d8cd48ec8c7b9
Nishant <nishant040305@gmail.com>
2024-07-25 15:21:52 +0530

    feat: Add a component for friend request with accept and reject feature
    Add the FriendRequest component which takes the emits a
    @friend-request/accept - to accept a request <notification ,
    userInfo:{image:url,Username:String}
    @friend-request/reject - to reject a request <notification._id>
    
    props - notification<DATA>

--- client/src/components/DashboardComponent/Notification/Notification.jsx
+++ client/src/components/DashboardComponent/Notification/Notification.jsx
@@ -1,6 +1,33 @@
-import React from 'react';
+import React ,{useEffect}from 'react';
 import './Notification.css';
+import { useSelector,useDispatch } from 'react-redux';
+import { addNotification ,removeNotification} from '../../../Store/NotificationSlice';
+import {socket} from '../../../Lib/socket'
+const FriendRequest = (props)=>{
+  const dispatch = useDispatch();
+  const user = useSelector(state=>state.user);
+  const notification = useSelector(state=>state.notification);
 
+  const AcceptFriendRequest=()=>{
+  
+    socket.emit('friend-request/accept',{notification:notification[props.index],userInfo:{image:user.image,Username:user.Username}});
+    dispatch(removeNotification(props.index));
+  }
+  const RejectFriendRequest=()=>{
+    socket.emit('friend-request/reject',{notification:notification[props.index]._id});
+    dispatch(removeNotification(props.index));
+  }
+  return(
+    <div className='Notification-friend Notification-message '>
+      <div className='flex flex-row ' style={{alignItems:'center'}}>
+      <img className='rounded-full w-9 h-9 mr-4' src={props.message.image}></img>{props.message.Username} wants to Friend with you
+      </div>
+      <div className='flex flex-row mt-3 ml-14'>
+        <button className='btn btn-success w-24 mr-10' onClick={()=>AcceptFriendRequest()}>Accept+</button><button className='btn btn-danger w-24' onClick={()=>RejectFriendRequest()}>Reject</button>
+      </div>
+    </div>
+  )
+}
 const NotificationMess =(props)=>{
     return(
         <div className='Notification-message'>
@@ -9,17 +36,21 @@ const NotificationMess =(props)=>{
         </div>
     )
 }
-
 const Notification = () => {
+  const notification = useSelector(state=>state.notification);
+  console.log(notification);
+  const dispatch = useDispatch();
+
+   
+
+   
   return (
     <div className='Notification'>
       <div className='Notification-head '>Notification</div>
       <div className='Notification-container'>
-        <NotificationMess text="hello my name is nishant mohan i study in this college slfjsldfj
-        ssfjks sdfjsd
-        sdflksjf sjdfkso fskldjs skfjls skdjfl sjdfkl lksjdf sljkdf s hello my name is nishant mohan i study in this college slfjsldfj
-        ssfjks sdfjsd
-        sdflksjf sjdfkso fskldjs skfjls skdjfl sjdfkl lksjdf sljkdf s"></NotificationMess>
+        {notification.map((info,index)=>(
+          (info.NotifType=='friend-request/send')?<FriendRequest key={index} _id={info._id} index = {index} SenderId={info.SenderId} message={info.message}></FriendRequest>:<></>
+        ))}
       </div>
     </div>
   )
